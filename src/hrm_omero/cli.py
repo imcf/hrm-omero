129a9029d2814cad9c9a325f6eb6ad503d028769
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2021-09-17 21:45:48 +0200

    Coding conventions

--- src/hrm_omero/cli.py
+++ src/hrm_omero/cli.py
@@ -189,16 +189,16 @@ def run_task(args):
     # TODO: implement requesting groups via cmdline option
 
     try:
-        if args.action == 'checkCredentials':
+        if args.action == "checkCredentials":
             return omero.check_credentials(conn)
-        elif args.action == 'retrieveChildren':
+        elif args.action == "retrieveChildren":
             return formatting.print_children_json(conn, args.id)
-        elif args.action == 'OMEROtoHRM':
+        elif args.action == "OMEROtoHRM":
             return transfer.from_omero(conn, args.imageid, args.dest)
-        elif args.action == 'HRMtoOMERO':
+        elif args.action == "HRMtoOMERO":
             return transfer.to_omero(conn, args.dset, args.file)
         else:
-            raise Exception('Huh, how could this happen?!')
+            raise Exception("Huh, how could this happen?!")
     finally:
         conn.close()
 
