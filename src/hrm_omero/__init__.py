485ec7036f983de5f81099ab2943bc0e6a5305df
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2021-09-20 15:03:26 +0200

    Include DEVELOPMENT and API-CHANGES for pdoc as well

--- src/hrm_omero/__init__.py
+++ src/hrm_omero/__init__.py
@@ -1,6 +1,10 @@
 """OMERO connector for the Huygens Remote Manager (HRM).
 
 .. include:: ../../README.md
+
+.. include:: ../../DEVELOPMENT.md
+
+.. include:: ../../API-CHANGES.md
 """
 
 __version__ = "0.3.1"
