cb2b654b07a5a31bc87abf687c485bbfed588fa7
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2021-10-01 22:44:19 +0200

    Add a '--online' option (marker) to prepare tests against a real OMERO

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,28 @@ import pytest
 from _pytest.logging import caplog as _caplog
 from loguru import logger
 
+
+### pytest setup ###
+
+def pytest_addoption(parser):
+    """Add a command line option '--online' to pytest."""
+    parser.addoption('--online', action='store_true', default=False,
+                     help="enable online tests communicating to a real OMERO instance")
+
+
+def pytest_collection_modifyitems(config, items):
+    """Add the 'skip' marker to tests decorated with 'pytest.mark.online'."""
+    if config.getoption("--online"):
+        # --online given in cli: do not skip online tests
+        return
+    skip_online = pytest.mark.skip(reason="need --online option to run")
+    for item in items:
+        if "online" in item.keywords:
+            item.add_marker(skip_online)
+
+
+### caplog special loguru setup ###
+
 @pytest.fixture
 def caplog(_caplog):
     """Fixture adding a sink that propagates Loguru messages to the builtin logging.
