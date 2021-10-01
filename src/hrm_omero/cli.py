df48568e98be25307b9aaa9cce7674e935125640
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2021-10-02 00:22:30 +0200

    Add a FIXME note

--- src/hrm_omero/cli.py
+++ src/hrm_omero/cli.py
@@ -308,6 +308,8 @@ def run_task(args):
             return True
 
         conn.connect()
+        # FIXME: the call below fails with an Ice.ConnectionLostException in case the
+        # password is wrong or something else failed:
         group = conn.getGroupFromContext()
         log.info(f"New OMERO connection [user={args.user}].")
         log.debug(f"The user's default group is {group.getId()} ({group.getName()}).")
