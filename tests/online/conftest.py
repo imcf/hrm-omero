9550e2826bf0eb92ee6ae98c45feb832de2d33ea
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2021-10-13 16:44:49 +0200

    Update docstring of "omero_conn" fixture

--- tests/online/conftest.py
+++ tests/online/conftest.py
@@ -78,27 +78,13 @@ def omeropw(monkeypatch, settings):
 
 @pytest.fixture
 def omero_conn(reach_tcp_or_skip, settings, omeropw):
-    """Establish a connection to on OMERO instance.
+    """Establish a connection to on OMERO instance or skip the test.
 
-    The fixture will try to import the test settings file or skip the entire test in
-    case importing fails.
-
-    Then it will try to figure out a password to use for the OMERO connection with the
-    entry "password" from the settings file having highest priority. In case that entry
-    doesn't exist, it will check if the environment has a variable 'OMERO_PASSWORD' and
-    use that one instead.
-
-    If none of the above methods to establish the password works, the entire test will
-    be skipped.
-
-    Then it will prepare the connection using the values from the test settings file and
-    finally will try to establish the connection. In case that step fails again the
-    entire test will be skipped.
+    The fixture will prepare a connection using the values from the test
+    settings file and finally will try to establish the connection. In case this
+    fails `pytest.skip()` will be called to skip the test.
 
     Eventually the connection object will be properly closed again.
-
-    If the test is skipped for any of the above described reasons a corresponding
-    message will be shown explaining the reason for skipping the test.
     """
 
     conn = omero.gateway.BlitzGateway(
