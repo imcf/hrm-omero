4666aa880bbee02ec8aa03efa5c224d6e288d7ee
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2021-12-15 23:25:12 +0100

    Add a sample script with values serving the OMERO preparation script

---
+++ resources/scripts/prepare-omero-values.inc.sh
@@ -0,0 +1,6 @@
+# preparation script values for testing HRM-OMERO
+SERVER=local-omero # make sure it resolves through /etc/hosts
+OMERO_USER=root
+OMERO_PASSWORD=omero_root_password
+U1_PW=H011aaaaW41dFee
+U2_PW=Alt3rKl4baut3r
