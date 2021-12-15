245467039dbcb2fc2197f49062755c6a32952dbc
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2021-12-15 23:26:31 +0100

    Make site-specific example match values from the preparation script

--- tests/resources/settings/site_specific.py
+++ tests/resources/settings/site_specific.py
@@ -1,22 +1,23 @@
 """Site-specific settings for the online tests."""
 
 
-HOSTNAME = "omero.example.xy"  # the OMERO server IP address or hostname
+HOSTNAME = "local-omero"  # the OMERO server address
 PORT = None  # the port to connect to the OMERO server (default= 4064)
 USERNAME = "hrm-test-01"  # a valid username on the OMERO server
-PASSWORD = None  # 'None' means the OMERO_PASSWORD environment variable will be used
-# PASSWORD = "w41dFee"  # the corresponding password for the OMERO user
+# PASSWORD = None  # 'None' means the OMERO_PASSWORD environment variable will be used
+PASSWORD = "H011aaaaW41dFee"  # the corresponding password for the OMERO user
 # USERNAME = "hrm-test-02",  # a valid username on the OMERO server
-# PASSWORD = "H011aaaa",  # the corresponding password for the OMERO user
+# PASSWORD = "Alt3rKl4baut3r",  # the corresponding password for the OMERO user
 
 
 IDS_TO_REPLACE = [
-    ("UID_1", "5809"),
-    ("UID_2", "5810"),
-    ("GID_1", "9"),
-    ("GID_2", "903"),
-    ("U1__PID_1", "Project:12221"),
-    ("U1__PID_1__DSID_1", "Dataset:47119"),
-    ("U1__PID_1__DSID_2", "Dataset:47120"),
-    ("U1__DSID_1", "Dataset:47121"),
+    ("UID_1", "2"),
+    ("UID_2", "3"),
+    ("GID_1", "3"),
+    ("GID_2", "4"),
+    ("U1__PID_1", "Project:1"),
+    ("U1__PID_1__DSID_1", "Dataset:1"),
+    ("U1__PID_1__DSID_2", "Dataset:2"),
+    ("U1__DSID_1", "Dataset:3"),
+    ("U1__IID_1", "Image:1"),
 ]
