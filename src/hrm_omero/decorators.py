a05d4f35bac09fe16c102fda46cb1590eba659f0
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2021-12-17 22:57:22 +0100

    Alter the `connect_and_set_group` decorator to use `OmeroId` objects
    This changes the decorator so it doesn't modify the number of parameters passed
    to the called function any more - instead it now makes sure the second argument
    has the right type (OmeroId) or tries to parse it in case a `str` is given.

--- src/hrm_omero/decorators.py
+++ src/hrm_omero/decorators.py
@@ -3,7 +3,7 @@
 import functools
 from loguru import logger as log
 
-from .misc import parse_id_str
+from .misc import OmeroId
 
 
 def connect_and_set_group(func):
@@ -13,41 +13,38 @@ def connect_and_set_group(func):
     connection to OMERO as well as the group for the connection / session to be switched
     explicitly.
 
-    In addition it also validates the `id_str` parameter to make sure only values are
-    accepted that will potentially work with the respective OMERO calls.
-
-    IMPORTANT: this decorator alters the signature of the function that is called - it
-    is **required** for the function to be decorated to have the first three parameters
-    of its signature to be `conn, obj_type, obj_id` as described in the **Other
-    Parameters** section below.
+    In addition it also checks the `omero_id` parameter and ensures it is an
+    object of type `hrm_omero.misc.OmeroId`. In case the parameter received is a
+    string, it will automatically create the corresponding `OmeroId` object,
+    which also checks the values of the ID for sanity.
 
     Other Parameters
     ----------------
     conn : omero.gateway.BlitzGateway
         The OMERO connection object.
-    obj_type : str
-        A valid OMERO object type, e.g. `Image` or `Dataset`.
-    obj_id : str or int
-        An OMERO object id, e.g. `1573559`.
+    omero_id : str or hrm_omero.misc.OmeroId
+        The fully qualified ID of an OMERO object (e.g. `G:23:Image:42`) as
+        a string or as an `hrm_omero.misc.OmeroId` object directly.
     """
 
     @functools.wraps(func)
-    def wrapper_connect_and_set_group(conn, id_str, *args, **kwargs):
+    def wrapper_connect_and_set_group(conn, omero_id, *args, **kwargs):
         """Inner wrapper function for the `connect_and_set_group` decorator.
 
         Parameters
         ----------
         conn : omero.gateway.BlitzGateway
             The OMERO connection object.
-        id_str : str
-            The ID of an OMERO object (e.g. `G:23:Image:42`).
+        omero_id : str or hrm_omero.misc.OmeroId
+            The fully qualified ID of an OMERO object (e.g. `G:23:Image:42`) as
+            a string or as an `hrm_omero.misc.OmeroId` object directly.
 
         Raises
         ------
         RuntimeError
             Raised in case the OMERO connection can't be established.
         ValueError
-            Raised in case a malformed `id_str` was given.
+            Raised in case a malformed `omero_id` was given.
         """
         # the connection might be closed (e.g. after importing an image), so force
         # re-establish it (note that `conn._connected` might even report the wrong
@@ -57,12 +54,14 @@ def connect_and_set_group(func):
             raise RuntimeError("Failed to (re-)establish connection to OMERO!")
         log.success("Successfully (re-)connected to OMERO!")
 
-        group_id, obj_type, obj_id = parse_id_str(id_str)
+        # if the ID is passed as a string parse it into an OmeroId object:
+        if isinstance(omero_id, str):
+            omero_id = OmeroId(omero_id)
 
         # set the OMERO group for the current connection session:
-        conn.setGroupForSession(group_id)
-        log.debug(f"Set OMERO session group to [{group_id}].")
+        conn.setGroupForSession(omero_id.group)
+        log.debug(f"Set OMERO session group to [{omero_id.group}].")
 
-        return func(conn, obj_type, obj_id, *args, *kwargs)
+        return func(conn, omero_id, *args, *kwargs)
 
     return wrapper_connect_and_set_group
